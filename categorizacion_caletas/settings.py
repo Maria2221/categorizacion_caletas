b8012cd6b8ba54a06da27e091a3a182aa3cbca49
mguerrav <mguerrav@unsa.edu.pe>
2019-05-30 14:30:02 -0500

    Subida con mejoras de la version

--- categorizacion_caletas/settings.py
+++ categorizacion_caletas/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'categorizacion_caletas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -90,7 +90,18 @@ DATABASES = {
         'HOST': 'ec2-54-243-197-120.compute-1.amazonaws.com',
         'PORT': '5432',
     }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7c5c7sdblt33n',
+        'USER': 'bxdhpojpwibbca',
+        'PASSWORD': '0e010c34261928e7fbae40ce480c463e7497b9580598b24b577c60dd8ff0fccb',
+        'HOST': 'ec2-54-163-226-238.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
